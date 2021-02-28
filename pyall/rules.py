64bc8a4475a053601e8513c806dc68386771c21e
Hakan Çelik <hakancelik96@outlook.com>
2021-02-28 12:13:57 +0300

    Mis Typing

--- pyall/rules.py
+++ pyall/rules.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import ast
 import functools
 from dataclasses import dataclass
-from typing import Callable, Iterator, cast
+from typing import Callable, ClassVar, Iterator, cast
 
 from pyall import constants as C
 from pyall.relate import first_occurrence
@@ -17,27 +17,26 @@ class InvalidRuleFunctionError(BaseException):
 
 @dataclass
 class Rule:
-    nodes: ast.AST
-    rule: Callable[[ast.AST], None]
+    nodes: tuple[ast.AST, ...]
+    rule: Callable[[ast.AST], bool]
+
+    rules: ClassVar[list[Rule]] = []
 
     @classmethod
-    def register(cls, nodes: ast.AST | tuple[ast.AST]) -> C.Function:
-        if not (nodes, tuple):
-            nodes = (nodes,)
-
+    def register(cls, nodes: tuple[ast.AST, ...]) -> C.Function:
         def f(rule: C.Function) -> None:
             cls.validate_rule(rule)
-            if not hasattr(cls, "rules"):
-                cls.rules = []
             cls.rules.append(cls(nodes=nodes, rule=rule))
 
         return cast(C.Function, f)
 
     @classmethod
-    def filter_by_node(cls, node) -> Iterator[Rule.rule]:
+    def filter_by_node(
+        cls, node: tuple[ast.AST, ...]
+    ) -> Iterator[Callable[[ast.AST], bool]]:
         for rule in cls.rules:
-            if isinstance(node, rule.nodes):
-                yield rule.rule
+            if isinstance(node, rule.nodes):  # type: ignore
+                yield rule.rule  # type: ignore
 
     @classmethod
     def apply_rules(cls, func: C.Function) -> C.Function:
@@ -55,7 +54,7 @@ class Rule:
         return cast(C.Function, wrapper)
 
     @classmethod
-    def validate_rule(cls, rule: Rule.rule) -> bool:
+    def validate_rule(cls, rule: Callable[[ast.AST], bool]) -> bool:
         if not rule.__name__.startswith("_rule_"):
             raise InvalidRuleFunctionError(
                 "Rule function name must start with '_rule_'."
@@ -73,43 +72,43 @@ class Rule:
 
 
 @Rule.register((ast.ClassDef, ast.FunctionDef, ast.AsyncFunctionDef, ast.Name))
-def _rule_node_skip(node):
+def _rule_node_skip(node) -> bool:
     return not node.skip
 
 
 @Rule.register((ast.ClassDef, ast.FunctionDef, ast.AsyncFunctionDef))
-def _rule_def_name(node):
+def _rule_def_name(node) -> bool:
     return not node.name.startswith("_")
 
 
 @Rule.register((ast.ClassDef, ast.FunctionDef, ast.AsyncFunctionDef, ast.Name))
-def _rule_parent_not_def(node):
+def _rule_parent_not_def(node) -> bool:
     return not first_occurrence(
         node, (ast.ClassDef, ast.FunctionDef, ast.AsyncFunctionDef)
     )
 
 
-@Rule.register(ast.Name)
-def _rule_name_name(node):
+@Rule.register((ast.Name,))
+def _rule_name_name(node) -> bool:
     return (
         node.id.isupper() or node.id[0].isupper()
     ) and not node.id.startswith("_")
 
 
-@Rule.register(ast.Name)
-def _rule_name_ctx(node):
+@Rule.register((ast.Name,))
+def _rule_name_ctx(node) -> bool:
     return isinstance(node.ctx, ast.Store)
 
 
-@Rule.register(ast.Assign)
-def _rule_node_is_all(node):
+@Rule.register((ast.Assign,))
+def _rule_node_is_all(node) -> bool:
     return getattr(node.targets[0], "id", None) == "__all__" and isinstance(
         node.value, (ast.List, ast.Tuple, ast.Set)
     )
 
 
-@Rule.register(ast.Expr)
-def _rule_node_is_all_item(node):
+@Rule.register((ast.Expr,))
+def _rule_node_is_all_item(node) -> bool:
     return (
         isinstance(node.value, ast.Call)
         and isinstance(node.value.func, ast.Attribute)
