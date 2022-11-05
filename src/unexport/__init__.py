77eb683aacbd1be9590c2f7a9c23fcdd9dc6a99d
Hakan Celik <hakancelikdev@gmail.com>
2022-11-05 23:59:52 +0300

    Upgrade version in action

--- src/unexport/__init__.py
+++ src/unexport/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ("__version__", "__description__")
 
-__version__ = "0.3.5"  # unexport: public
+__version__ = "0.4.0"  # unexport: public
 __description__ = (  # unexport: public
     "Unexport is a linter that tries to keep the __all __ in your Python modules always up to date."  # noqa: E501
 )
